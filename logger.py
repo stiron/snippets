d64e76aa3014be556e94a303c1f2ca88dc96afb6
Tamas Molnar <stiron@gmail.com>
2015-09-15 14:44:49 +0200

    Similar setup to the logger.pl

--- logger.py
+++ logger.py
@@ -2,30 +2,27 @@
 
 import logging
 
-logfile = 'program.log'
+logfile = 'my_log.log'
+log_layout_template = logging.Formatter(
+    '%(asctime)s %(levelname)s %(message)s',
+    "%Y-%m-%d-%H:%M:%s"
+)
+
 logger = logging.getLogger()
 
 # Root logger
 logger.setLevel(logging.NOTSET)
 
-# Console log handler
-console = logging.StreamHandler()
-console.setLevel(logging.INFO)
-formatter = logging.Formatter(
-    '%(asctime)s %(levelname)s %(message)s',
-    "%Y-%m-%d-%H:%M:%s"    
-)
-console.setFormatter(formatter)
-logger.addHandler(console)
+# Screen log handler
+screen = logging.StreamHandler()
+screen.setLevel(logging.INFO)
+screen.setFormatter(log_layout_template)
+logger.addHandler(screen)
 
 # File log handler
 file = logging.FileHandler(logfile)
 file.setLevel(logging.DEBUG)
-formatter = logging.Formatter(
-    '%(asctime)s %(levelname)s %(message)s',
-    "%Y-%m-%d-%H:%M:%s"    
-)
-file.setFormatter(formatter)
+file.setFormatter(log_layout_template)
 logger.addHandler(file)
 
 logging.info("Info message goes to file and screen")
