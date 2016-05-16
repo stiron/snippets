1b6a06a3a376468f1121be2a8684477a8c335368
Tamas Molnar <stiron@gmail.com>
2016-05-16 18:40:29 +0200

    Fixed an error around my variable spelling

--- logger.pl
+++ logger.pl
@@ -5,7 +5,7 @@ use warnings;
 use Log::Log4perl;
 
 my $log_layout          = 'Log::Log4perl::Layout::PatternLayout';
-my $log_layout_template = '%d{ISO8601} [%p] %m{chomp}%n';
+my $log_layout_template = '%d{ISO8601} [%C] %m{chomp}%n';
 my $log_file            = 'my_log.log';
 
 my $log_conf = qq(
@@ -14,14 +14,14 @@ my $log_conf = qq(
     log4perl.appender.Screen = Log::Log4perl::Appender::Screen
     log4perl.appender.Screen.stderr = 0
     log4perl.appender.Screen.layout = $log_layout
-    log4perl.appender.Screen.layout.ConversionPattern = $log_template
+    log4perl.appender.Screen.layout.ConversionPattern = $log_layout_template
     log4perl.appender.Screen.Threshold = INFO
 
     log4perl.appender.File = Log::Log4perl::Appender::File
     log4perl.appender.File.mode = write
     log4perl.appender.File.filename = $log_file
     log4perl.appender.File.layout = $log_layout
-    log4perl.appender.File.layout.ConversionPattern = $log_template
+    log4perl.appender.File.layout.ConversionPattern = $log_layout_template
 );
 
 Log::Log4perl->init( \$log_conf );
